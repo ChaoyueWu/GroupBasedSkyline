46ecd4fbc9617b5047f1833794ac502b369d4695
ly1996 <2958058357@qq.com>
2017-11-01 11:39:21 +0800

    modifing test.java

--- GroupBasedSkyline/src/thu/course/mds/project2/Test.java
+++ GroupBasedSkyline/src/thu/course/mds/project2/Test.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.naming.spi.DirStateFactory.Result;
-
 public class Test {
 	public static void main(String[] args) {
 		List<Point> points  = new ArrayList<Point>();
@@ -34,70 +32,28 @@ public class Test {
 		
 		d = points.get(0).getAttributes().length;
 		
-//		double[]d = {4,400};
-//		Point p = new Point(d);
-//		points.add(p);
-//		double[]d1 = {24,380};
-//		Point p1 = new Point(d1);
-//		points.add(p1);
-//		double[]d2 = {14,340};
-//		Point p2 = new Point(d2);
-//		points.add(p2);
-//		double[]d3 = {36,300};
-//		Point p3 = new Point(d3);
-//		points.add(p3);
-//		double[]d4 = {26,280};
-//		Point p4 = new Point(d4);
-//		points.add(p4);
-//		double[]d5 = {8,260};
-//		Point p5 = new Point(d5);
-//		points.add(p5);
-//		double[]d6 = {40,200};
-//		Point p6 = new Point(d6);
-//		points.add(p6);
-//		double[]d7 = {20,180};
-//		Point p7 = new Point(d7);
-//		points.add(p7);
-//		double[]d8 = {34,140};
-//		Point p8 = new Point(d8);
-//		points.add(p8);
-//		double[]d9 = {28,120};
-//		Point p9 = new Point(d9);
-//		points.add(p9);
-//		double[]d10 = {16,60};
-//		Point p10 = new Point(d10);
-//		points.add(p10);
-		
-		
 		DSGGenerator sg = new DSGGenerator(4,points,d);
-//		
-//		DSG dsg = sg.generateDSG();
+		
+		ProcessResult dsg = sg.generateDSG();
 //		for(DSGNode node:dsg.DSG){
 //			node.showNode();
 //		}
-//		PointWise pointWise = new PointWise();
-//		List<PointWise_Group> groups = pointWise.pointWiseCalculate(4, dsg);
-//		for(PointWise_Group group : groups) {
-//			System.out.println("group:");
-//			List<DSGNode> list = group.list;
-//			for(DSGNode node : list) {
-//				node.showNode();
-//			}
-//		}
+		
+		long startTime = System.nanoTime();//毫微秒
+		PointWise pointWise = new PointWise();
+		int groupsSize = pointWise.pointWiseCalculate(4, dsg);
+		long endTine = System.nanoTime();//毫微秒
+		System.out.println("point wise size: "+ groupsSize);
+		System.out.println("point wise time : " +(endTine - startTime)/1000);
+		
 		
 		//unit wise
 		UnitWise uw = new UnitWise();
-		List<List<Integer>> result = uw.unitWiseCalculate(4, sg.generateDSG().DSG);
-		
-		for (List<Integer> rr : result) {
-			for (Integer r : rr) {
-				System.out.print(r.intValue() + "  ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println(result.size());
-		
+		startTime = System.nanoTime();//毫微秒
+		List<List<Integer>> result = uw.unitWiseCalculate(4, dsg.DSG);
+		endTine = System.nanoTime();//毫微秒
+		System.out.println("unit wise size "+result.size());
+		System.out.println("point wise time : " +(endTine - startTime)/1000);
 	}
 
 }
